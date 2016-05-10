864501f562e02dc51be38dd3141555fa9fa55321
kkristof21 <kkristof21@hotmail.com>
2016-05-10 13:33:05 +0200

    Számjavítás
    Más számok.

--- github.cpp
+++ github.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
 cout << "Hello World!" << endl;
 int a = 21;
-a = 12 * 21;
-cout << a << endl;
+a = 12 * 12;
+cout << "Birthday multiply: " << a << endl;
 return 0;
 }
